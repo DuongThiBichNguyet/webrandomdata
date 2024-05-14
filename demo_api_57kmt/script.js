78f8f2b26ec0be1d3790054ad191554804120b6b
A D M I N <duongt.bnguyet@gmail.com>
2024-05-15 02:34:56 +0700

    Add project files.

---
+++ demo_api_57kmt/script.js
@@ -0,0 +1,26 @@
+﻿
+$(document).ready(function () {
+    $.get("http://127.0.0.1:1880/history", function (data) {
+        var a = [["Time", "Temp"]];
+        for (var i of data.data) {
+            var time = i.time;
+            var value = parseInt(i.value);
+            a.push([time, value]);
+        }
+        google.charts.load('current', { 'packages': ['corechart'] });
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable(a);
+
+            var options = {
+                title: 'Lịch sử',
+                hAxis: { title: 'Hours', titleTextStyle: { color: '#333' } },
+                vAxis: { minValue: 0 }
+            };
+
+            var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
+        }
+    });
+})
